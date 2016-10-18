d845d542631a711103d2e37e92be9588957dea3f
Michael Batz <mail@michael-batz.de>
2016-10-18 12:52:03 +0200

    forwarder: use variables

--- forwarder.py
+++ forwarder.py
@@ -1,5 +1,7 @@
 """Forwarding module for opennms_alarmforwarder"""
 
+import logging
+import re
 import sys
 from collections import OrderedDict
 
@@ -10,6 +12,7 @@ class Forwarder(object):
     def __init__(self, name, parameters):
         self._name = name
         self._parameters = parameters
+        self._logger = logging.getLogger("forwarder")
         
     def create_forwarder(name, classname, parameter):
         classobj = getattr(sys.modules[__name__], classname)
@@ -18,6 +21,29 @@ class Forwarder(object):
     def get_default_parameters(classname):
         classobj = getattr(sys.modules[__name__], classname)
         return classobj.default_parameters
+
+    def get_parameter(self, name):
+        value = ""
+        try:
+            value = type(self).default_parameters[name]
+            for parameter in self._parameters:
+                if parameter.parameter_name == name:
+                    value = parameter.parameter_value
+        except:
+            pass
+        return value
+
+    def substitute_alarm_variables(self, input_string, alarm):
+        def substitute_var(match):
+            replaced_var = match.group(0)
+            try:
+                replaced_var = str(getattr(alarm, match.group(1)))
+            except:
+                pass
+            return replaced_var
+        output = input_string
+        output = re.sub("%(alarm_.*?)%", substitute_var, input_string)
+        return output
 
     def test_forwarder(self):
         pass
@@ -32,15 +58,17 @@ class Forwarder(object):
 class StdoutForwarder(Forwarder):
 
     default_parameters = OrderedDict([
-        ("AlertMessage", "Forward alarm: %alarm_logmessage%"),
-        ("ResolvedMessage", "Resolve alarm: %alarm_logmessage%")
+        ("AlertMessage", "Forward alarm: %alarm_timestamp% %alarm_logmsg%"),
+        ("ResolvedMessage", "Resolve alarm: %alarm_timestamp% %alarm_logmsg%")
     ])
 
     def test_forwarder(self):
         print("This is a test of the forwarder " + self._name)
 
     def forward_alarm(self, alarm):
-        print("Forward alarm: " + str(alarm))
+        alarm_string = self.substitute_alarm_variables(self.get_parameter("AlertMessage"), alarm)
+        print(alarm_string)
 
     def resolve_alarm(self, alarm):
-        print("Resolve alarm: " + str(alarm))
+        alarm_string = self.substitute_alarm_variables(self.get_parameter("ResolvedMessage"), alarm)
+        print(alarm_string)
